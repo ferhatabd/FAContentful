384cbab9b2d6917a2f5ba262cdc541db503ab992
Ferhat Abdullahoglu <abdullahoglu@sabanciuniv.edu>
2019-12-29 21:22:55 +0300

    Initial Commit

---
+++ FAContenful/FAContenful.h
@@ -0,0 +1,19 @@
+//
+//  FAContenful.h
+//  FAContenful
+//
+//  Created by Ferhat Abdullahoglu on 29.12.2019.
+//  Copyright © 2019 Ferhat Abdullahoglu. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for FAContenful.
+FOUNDATION_EXPORT double FAContenfulVersionNumber;
+
+//! Project version string for FAContenful.
+FOUNDATION_EXPORT const unsigned char FAContenfulVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FAContenful/PublicHeader.h>
+
+
